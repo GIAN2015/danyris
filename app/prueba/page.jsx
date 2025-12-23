de99bc7f8070927f29d7c80a282507a2a98981de
GIAN2015 <giansica29@mail.com>
2025-12-23 14:26:02 -0500

    first commit

---
+++ app/prueba/page.jsx
@@ -0,0 +1,8 @@
+export default function PruebaPage() {
+  return(
+    <div>
+      <h1>Página de Prueba</h1>
+      <p>Esta es una página de prueba en la aplicación Next.js.</p>
+    </div>
+  )
+}
