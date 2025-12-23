c5e06368db8c51d177f5bd7a8850eea54c810712
Luis Vilchez <luis.vilchez@danyris.com>
2025-12-23 14:53:10 -0500

    cambio

--- app/prueba/page.jsx
+++ app/prueba/page.jsx
@@ -3,6 +3,7 @@ export default function PruebaPage() {
     <div>
       <h1>Página de Prueba</h1>
       <p>Esta es una página de prueba en la aplicación Next.js.</p>
+      <h1>cambio de git </h1>
     </div>
   )
 }
