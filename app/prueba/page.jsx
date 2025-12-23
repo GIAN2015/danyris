873afa7daa0f28135c9d7618d6083a5fe1cdb4df
Gian Sinarahua Cárdenas <74512214+GIAN2015@users.noreply.github.com>
2025-12-23 15:03:58 -0500

    Delete app/prueba directory

--- app/prueba/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function PruebaPage() {
-  return(
-    <div>
-      <h1>Página de Prueba</h1>
-      <p>Esta es una página de prueba en la aplicación Next.js.</p>
-      <h1>cambio de git </h1>
-    </div>
-  )
-}
